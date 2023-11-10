25fa942b23ad0abde9e5f8fc5be3e79d9d910374
coconutonsm <coconutonsm@gmail.com>
2023-11-10 20:39:56 +0300

    fix: change pagination for fix bugs

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,7 +6,7 @@ import Preloader from '../components/Preloader/Preloader';
 import Pagination from '../components/Pagination/Pagination';
 import LimitCards from '../components/LimitCards/LimitCards';
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { getCards } from '../services/getData';
+import { getAllSearchCards, getCards } from '../services/getData';
 import { ALL_BEER, API_URL } from '../consts';
 
 const App = () => {
@@ -17,18 +17,30 @@ const App = () => {
   const [perPage, setPerPage] = useState<number>(8);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [cards, setCards] = useState<resultItem[]>([]);
+  const [allCardsForSearch, setAlCardsForSearch] = useState<resultItem[]>();
   const [, setSearchParams] = useSearchParams();
 
   const getSearchText = (searchText: string) => {
     setInputValue(searchText);
   };
 
-  useEffect(() => {
-    let url = API_URL + `?per_page=${perPage}&page=${currentPage}`;
-
-    if (inputValue) {
-      url = inputValue ? API_URL + `?beer_name=${inputValue}` : API_URL;
+  const setAllSearchCards = async (url: string) => {
+    const allItemsForSearch = await getAllSearchCards(url);
+    if (allItemsForSearch) {
+      setAlCardsForSearch(allItemsForSearch);
     }
+  };
+
+  useEffect(() => {
+    let url = inputValue
+      ? API_URL + `?beer_name=${inputValue}&per_page=${80}`
+      : API_URL;
+    setAllSearchCards(url);
+
+    url =
+      API_URL +
+      `?per_page=${perPage}&page=${currentPage}${inputValue ? `&beer_name=${inputValue}` : ``
+      }`;
 
     getCards(url, setCards, setIsLoading);
     setIsLoading(true);
@@ -46,7 +58,11 @@ const App = () => {
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
               maxPage={Math.ceil(
-                (inputValue ? cards.length : ALL_BEER) / perPage
+                (inputValue
+                  ? allCardsForSearch
+                    ? allCardsForSearch.length
+                    : 0
+                  : ALL_BEER) / perPage
               )}
             />
             <div className="cards_wrapper">
--- src/services/getData.tsx
+++ src/services/getData.tsx
@@ -28,3 +28,25 @@ export const getCards = async (
     setIsLoading(true);
   }
 };
+
+export const getAllSearchCards = async (url: string) => {
+  try {
+    return axios
+      .get(url)
+      .then((response) => {
+        const cards: resultItem[] = response.data;
+        return cards;
+      })
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          console.log(err.message);
+        }
+      });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    } else if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+};
